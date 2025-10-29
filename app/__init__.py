e5f93f4ae9418fa2981949f1bcaaed1ab827e204
Gonzalo Inostroza <gonzalo-inostroza@live.cl>
2025-10-29 14:11:21 -0300

    Mockup recomendaciones

--- app/__init__.py
+++ app/__init__.py
@@ -27,3 +27,13 @@ def home():
     return render_template(
         "index.html", intereses=INTERESES_DISPONIBLES, asignaturas=ASIGNATURAS
     )
+
+
+methods = ["POST"]
+
+
+@app.route("/orientar", methods=methods)
+def api_orientar():
+    demo_data = ["DevOps", "Frontend"]
+
+    return render_template("_recomendaciones.html", recomendaciones=demo_data)
